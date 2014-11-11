e607dc5b54900c9132e2d7d2f9dd0a5b33f8dc97
Nicolas Bortolotti <nbortolotti@gmail.com>
2014-11-11 22:53:52 +0000

    updating examples and IF section

--- Examples/If/initial.py
+++ Examples/If/initial.py
@@ -1,6 +1,8 @@
 __author__ = 'nickbortolotti'
 #details: initial level example using IF structure
 
+'''
+#Case 1: using just a simple IF structure
 valor = raw_input("Ingresa un valor general")
 
 if valor < 10:
@@ -9,4 +11,28 @@ elif valor > 10:
     print "El valor es mayor a 10"
 else:
     print "no podemos indentificar el valor real"
-    
+'''
+
+'''
+#Case 2: comparing switch using dictionary
+seleccion = raw_input("Ingresa tu seleccion: ")
+print({'demo':1.50,
+       'demo1':2.00,
+       'demo3':2.50,
+       'demo4':3.00}[seleccion])
+'''
+
+'''
+#Case 3: replacing the Case 2 swicth to If structure
+seleccion = raw_input("Ingresa tu seleccion: ")
+
+if seleccion == 'demo':
+    print(1.50)
+elif seleccion == 'demo1':
+    print(2.00)
+elif seleccion == 'demo2':
+    print (3.00)
+else:
+    print('mala seleccion')
+'''
+
