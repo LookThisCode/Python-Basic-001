cfc01e263538c398651e7043923bc4c886b3dfbd
Nicolas Bortolotti <nbortolotti@gmail.com>
2014-11-11 03:22:41 +0000

    example if initial update

---
+++ Examples/If/initial.py
@@ -0,0 +1,12 @@
+__author__ = 'nickbortolotti'
+#details: initial level example using IF structure
+
+valor = raw_input("Ingresa un valor general")
+
+if valor < 10:
+    print "El valor en menor a 10"
+elif valor > 10:
+    print "El valor es mayor a 10"
+else:
+    print "no podemos indentificar el valor real"
+    
