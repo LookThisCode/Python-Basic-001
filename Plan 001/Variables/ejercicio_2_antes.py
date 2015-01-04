4a6ee9e11afd60d8795673d8662015d95eca3cad
Nicolas Bortolotti <nbortolotti@gmail.com>
2015-01-04 17:25:11 -0600

    para el plan 001 agregando ejercicio_2_antes, objetivo interaccion de plataformas

---
+++ Plan 001/Variables/ejercicio_2_antes.py
@@ -0,0 +1,11 @@
+__author__ = 'nickbortolotti'
+
+"""
+Parte 1
+1.utilizar un arreglo que almacene 2 cadenas y 2 valores enteros
+2.mostrar en pantalla el la ubicacion 1
+mostrar desde el 0-2
+mostrar del 2 en adelante
+mostrar del 1 en reversa
+mostrar el ultimo elemento del arreglo en reversa
+"""
