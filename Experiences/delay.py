ad9749e14864c2114d454d5984de62542eb7152a
Nicolas Bortolotti <nbortolotti@gmail.com>
2017-10-12 13:39:06 +0200

    added delay.py experience

---
+++ Experiences/delay.py
@@ -0,0 +1,10 @@
+import time
+
+# print "mensaje inicial antes de la espera"
+# time.sleep(15) # espera en segundos
+# print "mensaje luego de la espera"
+
+
+while True:
+    print("imprimir mensaje cada 5 segundos")
+    time.sleep(5)
